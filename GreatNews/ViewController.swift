04816d1004e315b5b7029c4abe2fa2fc36caaac2
Breno Vasconcellos <dv.brenovasconcellos@gmail.com>
2023-01-17 21:29:37 -0300

    first commit

---
+++ GreatNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreatNews
+//
+//  Created by Breno Vasconcellos on 17/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
